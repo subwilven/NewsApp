81d38182a229dd5630fec320f6e65fec9ffc8fe3
Islam <islam.m.mohamed95@gmail.com>
2023-02-25 14:14:09 +0200

    Run detekt on pre-commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,15 +16,21 @@ subprojects {
     }
 }
 
-tasks {
-    register("clean", Delete::class) {
-        delete(rootProject.buildDir)
-    }
-}
+
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.dagger.hilt) apply false
     alias(libs.plugins.kotlin.kapt) apply false
     alias(libs.plugins.detekt.plugin)
+}
+
+tasks {
+    register("clean", Delete::class) {
+        delete(rootProject.buildDir)
+    }
+}
+tasks.register<Copy>("copyGitHooksScripts") {
+    from(layout.projectDirectory.dir("scripts"))
+    into(layout.projectDirectory.dir(".git/hooks"))
 }
