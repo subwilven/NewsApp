b1b9ea848fd4a7963a25779e28b237b7fd389b80
Islam Mostafa <islam.m.mohamed95@gmail.com>
2023-08-26 17:41:32 +0300

    Create build.yaml

---
+++ app/src/test/java/com/example/newsapp/usecases/FetchArticlesUseCaseTest.kt
@@ -0,0 +1,12 @@
+package com.example.newsapp.usecases
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+internal class FetchArticlesUseCaseTest {
+
+    @Test
+    fun test() {
+        Assertions.assertTrue(true)
+    }
+}
