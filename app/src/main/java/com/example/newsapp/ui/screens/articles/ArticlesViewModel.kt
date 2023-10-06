3ae0e5f1ebb3c45699fce5d1fb6ffb9a6014e0e1
islammostafa <imohamed@innovationteam.com>
2023-10-06 10:23:51 +0300

    Fix Articles being re-fetched again after get back to the articles screen

--- app/src/main/java/com/example/newsapp/ui/screens/articles/ArticlesViewModel.kt
+++ app/src/main/java/com/example/newsapp/ui/screens/articles/ArticlesViewModel.kt
@@ -2,25 +2,24 @@ package com.example.newsapp.ui.screens.articles
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import androidx.paging.cachedIn
 import com.example.newsapp.data.providers.repository.ProvidersRepository
 import com.example.newsapp.model.FilterData
 import com.example.newsapp.model.articles.Article
 import com.example.newsapp.usecases.FetchArticlesUseCase
 import com.example.newsapp.usecases.ToggleFavoriteStateUseCase
 import com.example.newsapp.util.DEBOUNCE_SEARCH_INPUT
-import com.example.newsapp.util.FLOW_SUBSCRIPTION_TIMEOUT
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.debounce
 import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.flatMapLatest
-import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -33,39 +32,41 @@ class ArticlesViewModel @Inject constructor(
     private val toggleFavoriteStateUseCase: ToggleFavoriteStateUseCase,
 ) : ViewModel() {
 
-    private val searchInputFlow = MutableStateFlow<String?>(null)
+
+    private val _uiState = MutableStateFlow(ArticleUiState())
+    val uiState = _uiState.asStateFlow()
+
+    private val searchUserInputFlow = MutableStateFlow<String?>(null)
 
     private val filterDataFlow = combine(
-        searchInputFlow,
+        searchUserInputFlow,
         providersRepository.getSelectedProvidersIds()
     ) { inputSearch, selectedProviders ->
         FilterData(
             searchInput = inputSearch,
             selectedProvidersIds = selectedProviders
         )
+    }.onEach { filterData ->
+        _uiState.update {
+            it.copy(filterData = filterData)
+        }
     }
-    private val pagingListFlow = filterDataFlow
+
+    private val articlesPagingDataFlow = filterDataFlow
         .debounce(DEBOUNCE_SEARCH_INPUT)
         .distinctUntilChanged()
         .flatMapLatest { filterData ->
             fetchArticlesUseCase(filterData)
+        }.cachedIn(viewModelScope)
+
+    init {
+        _uiState.update {
+            it.copy(articlesDataFlow = articlesPagingDataFlow)
         }
-
-    val uiState: StateFlow<ArticleUiState> = filterDataFlow
-        .map(::mapToUiState)
-        .stateIn(
-            viewModelScope,
-            SharingStarted.WhileSubscribed(FLOW_SUBSCRIPTION_TIMEOUT),
-            ArticleUiState(articlesDataFlow = pagingListFlow)
-        )
-
-    private fun mapToUiState(filterData: FilterData) = ArticleUiState(
-        articlesDataFlow = pagingListFlow,
-        filterData = filterData
-    )
+    }
 
     fun searchByQuery(searchInput: String?) {
-        searchInputFlow.tryEmit(searchInput)
+        searchUserInputFlow.tryEmit(searchInput)
     }
 
     fun changeArticleFavoriteState(article: Article) {
